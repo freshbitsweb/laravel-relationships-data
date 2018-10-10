5cef5ee4d805262d23cca4af2a8eb9ec6ffa8498
Gaurav Makhecha <gauravmakhecha@gmail.com>
2018-10-10 13:07:56 +0530

    Work for has many through relationship

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    /**
+     * Get all of the posts for the country.
+     */
+    public function posts()
+    {
+        return $this->hasManyThrough('App\Post', 'App\User');
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Role;
+use App\Country;
 use Faker\Generator as Faker;
 
 /*
@@ -16,6 +17,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
+        'country_id' => Country::inRandomOrder()->value('id'),
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
